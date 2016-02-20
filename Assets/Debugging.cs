fb61aff222c1f454a6e37bc6d04b6e10fb4c1db2
unknown <laiafreixas9@gmail.com>
2016-02-21 00:41:53 +0100

    camera rotator with joystick

---
+++ Assets/Debugging.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Debugging : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.JoystickButton4)) Debug.Log (4);
+		if (Input.GetKeyDown(KeyCode.JoystickButton5)) Debug.Log (5);
+
+	}
+}
