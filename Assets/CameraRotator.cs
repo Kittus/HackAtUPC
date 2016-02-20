911899a752e64251ab3dc5e4ce3b77e45398f9a4
ImInSpace <ignigomoreno@hotmail.com>
2016-02-20 21:11:50 +0100

    Camera rotator

---
+++ Assets/CameraRotator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotator : MonoBehaviour {
+	public float speed;
+	public void Update (){
+		transform.Rotate (0, Input.GetAxisRaw ("Custom") * speed * Time.deltaTime, 0);
+	}
+}
