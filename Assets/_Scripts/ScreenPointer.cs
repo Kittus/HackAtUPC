1c7c99565627e39ee9285db2c3ceb431972bb333
acanya <arnau_canya@hotmail.com>
2016-02-20 15:52:26 +0100

    Pointer moves

--- Assets/_Scripts/ScreenPointer.cs
+++ Assets/_Scripts/ScreenPointer.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class ScreenPointer : MonoBehaviour {
 
+    public Camera troll;
+    public GameObject collision;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,16 @@ public class ScreenPointer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        Debug.Log("HOLA");
+        RaycastHit hit;
+        Ray ray = troll.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 10));
+        int layerMask = 1 << LayerMask.NameToLayer("Screen"); // only check for collisions with layerX
+        if (Physics.Raycast(ray,out hit, 700f, layerMask))
+        {
+            collision.GetComponent<Renderer>().enabled = true;
+            collision.transform.position = hit.point + new Vector3(0,0,collision.transform.position.z-hit.point.z);
+        }
+        else
+            collision.GetComponent<Renderer>().enabled = false;
+    }
 }
