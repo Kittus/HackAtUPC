1743084cd22a07def8c5a13d3ff379cfed11e16f
Kittus <markasenjo@hotmail.com>
2016-02-20 10:36:05 +0100

    The Man Who Can Now Move!

--- Assets/_Scripts/playerController.cs
+++ Assets/_Scripts/playerController.cs
@@ -1,35 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class playerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
     public float height = 2.0f;
     public float sensibility = 2.0f;
     public float speed = 6.0f;
+    public float timeScaleWhenStop = 0.05f;
+
+    public GameObject Parent;
 
 	// Use this for initialization
 	void Start () {
-        transform.Translate(0f, height, 0f);
+        Parent.transform.Translate(0f, height, 0f);
         Cursor.visible = false;
     }
-
-    // Update is called once per frame
-    /*void FixedUpdate () {
-        float h = Input.GetAxis("Mouse X") * Time.deltaTime * sensibility*20;
-        float v = Input.GetAxis("Mouse Y") * Time.deltaTime * sensibility*20;
-       
-       transform.Rotate (new Vector3(-v, h,0));
-   
-	}*/
+    
     private float yaw = 0.0f;
     private float pitch = 0.0f;
-    private bool timePass = false;
-
+    
     void Update()
     {
-        if (timePass) Time.timeScale = 1;
-        else Time.timeScale = 0.05f;
-
         yaw += sensibility * Input.GetAxis("Mouse X");
         pitch -= sensibility * Input.GetAxis("Mouse Y");
 
@@ -38,38 +29,38 @@ public class playerController : MonoBehaviour {
 
     void FixedUpdate()
     {
-        timePass = false;
+        Time.timeScale = timeScaleWhenStop;
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
+            Time.timeScale = 1;
             Vector3 v = transform.forward;
             v.y = 0f;
             v.Normalize();
-            transform.position += v;
-            timePass = true;
+            Parent.transform.position += v;
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
+            Time.timeScale = 1;
             Vector3 v = transform.forward;
             v.y = 0f;
             v.Normalize();
-            transform.position -= v;
-            timePass = true;
+            Parent.transform.position -= v;
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
+            Time.timeScale = 1;
             Vector3 v = transform.right;
             v.y = 0f;
             v.Normalize();
-            transform.position += v;
-            timePass = true;
+            Parent.transform.position += v;
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
+            Time.timeScale = 1;
             Vector3 v = transform.right;
             v.y = 0f;
             v.Normalize();
-            transform.position -= v;
-            timePass = true;
+            Parent.transform.position -= v;
         }
 
     }
