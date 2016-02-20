004b87f728860d4044ef126dc43a58cd13ad2e53
unknown <laiafreixas9@gmail.com>
2016-02-20 01:22:38 +0100

    rocks falling down into the ramp

---
+++ Assets/_Scripts/RockController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockController : MonoBehaviour
+{
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	public int BaseMass;
+	void Start ()
+	{
+		StartCoroutine (SpawnWaves ());
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+
+				float scale = Random.Range (1.0F, 5.0F);
+
+				hazard.transform.localScale = new Vector3(scale,scale,scale);
+				Rigidbody rb = hazard.GetComponent<Rigidbody>();
+				rb.mass = BaseMass * scale;
+
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+	}
+}
