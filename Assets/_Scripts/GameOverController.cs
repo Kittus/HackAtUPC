24dc8712dcfd828586e4b78bba565d9fb588c4e0
unknown <laiafreixas9@gmail.com>
2016-02-21 02:02:05 +0100

    canvis main Arnauu

--- Assets/_Scripts/GameOverController.cs
+++ Assets/_Scripts/GameOverController.cs
@@ -10,7 +10,10 @@ public class GameOverController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log ("aa");
+		if (Input.GetKeyDown(KeyCode.Joystick1Button2)){
+			LoadOnClick.LoadScene(0);
+		}
 	}
 
 	// Go to Main Menu
